19e09f1b4b4dfac323afdbc692cb940b0756542a
rafael <rmendesduarte@gmail.com>
2015-09-28 14:30:51 -0300

    remove OLD folder

--- OLD/catkin_ws/build/bci_training_platform/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hieron/catkin_ws/src/bci_training_platform
-# Build directory: /home/hieron/catkin_ws/build/bci_training_platform
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
