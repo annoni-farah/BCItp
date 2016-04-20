24853f3c47e4d65c87a7d2ca86d67d867aa400b7
Rafael Duarte <rafaelmd@gmail.com>
2016-04-20 14:33:45 -0300

    remove variables and funct not used

--- open_bci_simu.py
+++ open_bci_simu.py
@@ -9,38 +9,8 @@ import struct
 import numpy as np
 import time
 import timeit
-import atexit
-import logging
-import threading
-import sys
-import pdb
 
 SAMPLE_RATE = 250.0  # Hz
-START_BYTE = 0xA0  # start of data packet
-END_BYTE = 0xC0  # end of data packet
-ADS1299_Vref = 4.5  #reference voltage for ADC in ADS1299.  set by its hardware
-ADS1299_gain = 24.0  #assumed gain setting for ADS1299.  set by its Arduino code
-scale_fac_uVolts_per_count = ADS1299_Vref/float((pow(2,23)-1))/ADS1299_gain*1000000.
-scale_fac_accel_G_per_count = 0.002 /(pow(2,4)) #assume set to +/4G, so 2 mG 
-'''
-#Commands for in SDK http://docs.openbci.com/software/01-Open BCI_SDK:
-
-command_stop = "s";
-command_startText = "x";
-command_startBinary = "b";
-command_startBinary_wAux = "n";
-command_startBinary_4chan = "v";
-command_activateFilters = "F";
-command_deactivateFilters = "g";
-command_deactivate_channel = {"1", "2", "3", "4", "5", "6", "7", "8"};
-command_activate_channel = {"q", "w", "e", "r", "t", "y", "u", "i"};
-command_activate_leadoffP_channel = {"!", "@", "#", "$", "%", "^", "&", "*"};  //shift + 1-8
-command_deactivate_leadoffP_channel = {"Q", "W", "E", "R", "T", "Y", "U", "I"};   //letters (plus shift) right below 1-8
-command_activate_leadoffN_channel = {"A", "S", "D", "F", "G", "H", "J", "K"}; //letters (plus shift) below the letters below 1-8
-command_deactivate_leadoffN_channel = {"Z", "X", "C", "V", "B", "N", "M", "<"};   //letters (plus shift) below the letters below the letters below 1-8
-command_biasAuto = "`";
-command_biasFixed = "~";
-'''
 
 class OpenBCIBoard(object):
   """
@@ -57,31 +27,12 @@ class OpenBCIBoard(object):
     scaled_output=True, daisy=False, log=True, timeout=None):
 
     print("Connecting to V3 simulator")
-#    self.ser = serial.Serial(port= port, baudrate = baud, timeout=timeout)
-
-    time.sleep(2)
-
-
     #wait for device to be ready
     time.sleep(1)
 
     self.streaming = False
-    self.filtering_data = filter_data
     self.scaling_output = scaled_output
-    self.eeg_channels_per_sample = 8 # number of EEG channels per sample *from the board*
-    self.aux_channels_per_sample = 3 # number of AUX channels per sample *from the board*
-    self.read_state = 0
-    self.daisy = daisy
-    self.last_odd_sample = OpenBCISample(-1, [], []) # used for daisy
-    self.log_packet_count = 0
-    self.attempt_reconnect = False
-    self.last_reconnect = 0
-    self.reconnect_freq = 5
-    self.packets_dropped = 0
 
-    #Disconnects from board when terminated
-    atexit.register(self.disconnect)
-  
   def getSampleRate(self):
       return SAMPLE_RATE
 
@@ -140,77 +91,6 @@ class OpenBCIBoard(object):
   def warn(self, text):
       
     print("Warning: %s" % text)
- 
-
-  def set_channel(self, channel, toggle_position):
-    #Commands to set toggle to on position
-    if toggle_position == 1:
-      if channel is 1:
-        self.ser.write(b'!')
-      if channel is 2:
-        self.ser.write(b'@')
-      if channel is 3:
-        self.ser.write(b'#')
-      if channel is 4:
-        self.ser.write(b'$')
-      if channel is 5:
-        self.ser.write(b'%')
-      if channel is 6:
-        self.ser.write(b'^')
-      if channel is 7:
-        self.ser.write(b'&')
-      if channel is 8:
-        self.ser.write(b'*')
-      if channel is 9 and self.daisy:
-        self.ser.write(b'Q')
-      if channel is 10 and self.daisy:
-        self.ser.write(b'W')
-      if channel is 11 and self.daisy:
-        self.ser.write(b'E')
-      if channel is 12 and self.daisy:
-        self.ser.write(b'R')
-      if channel is 13 and self.daisy:
-        self.ser.write(b'T')
-      if channel is 14 and self.daisy:
-        self.ser.write(b'Y')
-      if channel is 15 and self.daisy:
-        self.ser.write(b'U')
-      if channel is 16 and self.daisy:
-        self.ser.write(b'I')
-    #Commands to set toggle to off position
-    elif toggle_position == 0:
-      if channel is 1:
-        self.ser.write(b'1')
-      if channel is 2:
-        self.ser.write(b'2')
-      if channel is 3:
-        self.ser.write(b'3')
-      if channel is 4:
-        self.ser.write(b'4')
-      if channel is 5:
-        self.ser.write(b'5')
-      if channel is 6:
-        self.ser.write(b'6')
-      if channel is 7:
-        self.ser.write(b'7')
-      if channel is 8:
-        self.ser.write(b'8')
-      if channel is 9 and self.daisy:
-        self.ser.write(b'q')
-      if channel is 10 and self.daisy:
-        self.ser.write(b'w')
-      if channel is 11 and self.daisy:
-        self.ser.write(b'e')
-      if channel is 12 and self.daisy:
-        self.ser.write(b'r')
-      if channel is 13 and self.daisy:
-        self.ser.write(b't')
-      if channel is 14 and self.daisy:
-        self.ser.write(b'y')
-      if channel is 15 and self.daisy:
-        self.ser.write(b'u')
-      if channel is 16 and self.daisy:
-        self.ser.write(b'i')
 
 
 class OpenBCISample(object):
