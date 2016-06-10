76dca0b4a42524230fe263d5d57bc1ac8a999a88
rafael <rmendesduarte@gmail.com>
2016-06-10 14:07:28 -0300

    add labels to precal settings

--- screens/precal/PreCalSettings.py
+++ screens/precal/PreCalSettings.py
@@ -16,47 +16,52 @@ class PreCalSettings(Screen):
 
         boxg = BoxLayout(orientation='vertical', padding=10, spacing=10)
 
-        box_bottom = BoxLayout(size_hint_x=1, size_hint_y=0.3, 
+        ## TOP PART
+        box_top = BoxLayout(size_hint_x=1, size_hint_y=0.7,
             padding=10, spacing=10, orientation='vertical')
 
-        self.label_msg = Label(text="", font_size=FONT_SIZE)
-        
-        button_save = Button(text="Save", size = BUTTON_SIZE)
-        button_save.bind(on_press= self.save_config)
-
-        button_default = Button(text="Load Default Config", size = BUTTON_SIZE)
-        button_default.bind(on_press= self.load_default_settings)
-
-        button_back = Button(text="Back", size = BUTTON_SIZE)
-        button_back.bind(on_press= self.change_to_cal)
-
-        box_top = BoxLayout(size_hint_x=1, size_hint_y=0.5,
-            padding=10, spacing=10, orientation='vertical')
-
-
+        box_tt = BoxLayout(orientation = 'horizontal')
+        label_tt = Label(text = 'Total Time (s)', font_size=FONT_SIZE)
         self.total_time = TextInput(size_hint=(1, 0.8), font_size= FONT_SIZE,
-                        hint_text='Total Time (s)', multiline=False)
+                        text='100', multiline=False)
+        box_tt.add_widget(label_tt)
+        box_tt.add_widget(self.total_time)
+        box_top.add_widget(box_tt)
 
+        box_rt = BoxLayout(orientation = 'horizontal')
+        label_rt = Label(text = 'Time to set Max (s)', font_size=FONT_SIZE)
         self.relax_time = TextInput(size_hint=(1, 0.8), font_size= FONT_SIZE,
-                        hint_text='Time to set Max (s)', multiline=False)
+                        text='10', multiline=False)
+        box_rt.add_widget(label_rt)
+        box_rt.add_widget(self.relax_time)
+        box_top.add_widget(box_rt)
 
+        box_ch = BoxLayout(orientation = 'horizontal')
+        label_ch = Label(text = 'Right - Channels used in Energy Calc:', font_size=FONT_SIZE)
         self.ch_energy_right = TextInput(size_hint=(1, 0.8), font_size= FONT_SIZE,
-                hint_text='Right - Channels to use in Energy Computation :1 2 3 ...)', multiline=False)
+                text= '0', multiline=False)
+        box_ch.add_widget(label_ch)
+        box_ch.add_widget(self.ch_energy_right)
+        box_top.add_widget(box_ch)
 
+        box_chl = BoxLayout(orientation = 'horizontal')
+        label_chl = Label(text = 'Left - Channels used in Energy Calc:', font_size=FONT_SIZE)
         self.ch_energy_left = TextInput(size_hint=(1, 0.8), font_size= FONT_SIZE,
-            hint_text='Left - Channels to use in Energy Computation :1 2 3 ...)', multiline=False)
+            text='1', multiline=False)
+        box_chl.add_widget(label_chl)
+        box_chl.add_widget(self.ch_energy_left)
+        box_top.add_widget(box_chl)
 
+        box_sd = BoxLayout(orientation = 'horizontal')
+        label_sd = Label(text = 'Sign - left or right', font_size=FONT_SIZE)
         self.sign_direction = TextInput(size_hint=(1, 0.8), font_size= FONT_SIZE,
-            hint_text='Sign - left or right', multiline=False)
+            text='left', multiline=False)
+        box_sd.add_widget(label_sd)
+        box_sd.add_widget(self.sign_direction)
+        box_top.add_widget(box_sd)
 
         checkbox_plot = CheckBox()
         checkbox_plot.bind(active=self.enable_plot)
-
-        box_top.add_widget(self.sign_direction)
-        box_top.add_widget(self.ch_energy_left)
-        box_top.add_widget(self.ch_energy_right)
-        box_top.add_widget(self.relax_time)
-        box_top.add_widget(self.total_time)
 
         label_plot = Label(text="Plot Data:", font_size=FONT_SIZE)
         box_checkbox = BoxLayout(orientation='horizontal')
@@ -66,14 +71,26 @@ class PreCalSettings(Screen):
 
         box_top.add_widget(box_checkbox)
 
+        boxg.add_widget(box_top)
+
+        ## BOTTOM PART
+        box_bottom = BoxLayout(size_hint_x=1, size_hint_y=0.3, 
+            padding=10, spacing=10, orientation='vertical')
+
+        self.label_msg = Label(text="", font_size=FONT_SIZE)
+        
+        button_save = Button(text="Save", size = BUTTON_SIZE)
+        button_save.bind(on_press= self.save_config)
+
+        button_back = Button(text="Back", size = BUTTON_SIZE)
+        button_back.bind(on_press= self.change_to_cal)
 
         box_bottom.add_widget(self.label_msg)
         box_bottom.add_widget(button_save)
-        box_bottom.add_widget(button_default)
         box_bottom.add_widget(button_back)
 
-        boxg.add_widget(box_top)
         boxg.add_widget(box_bottom)
+
 
         self.add_widget(boxg)
 
@@ -97,17 +114,6 @@ class PreCalSettings(Screen):
             data = json.load(data_file)
             session_list = data["session_list"]
             self.session = session_list[-1]
-
-    def load_default_settings(self,*args):
-        PATH_TO_DEFAULT = 'data/default_configs/precal_config.txt'
-
-        with open(PATH_TO_DEFAULT, "r") as data_file:    
-            data = json.load(data_file)
-            self.ch_energy_left.text = data["ch_energy_left"]
-            self.ch_energy_right.text = data["ch_energy_right"]
-            self.total_time.text = data["total_time"]
-            self.relax_time.text = data["relax_time"]
-            self.sign_direction.text = data["sign_direction"]
 
     def save_config(self,*args):
         self.load_session_config()
