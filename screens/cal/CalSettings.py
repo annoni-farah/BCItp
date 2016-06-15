2b765021dd871bf37bc24397dae5f2141a01b21e
rafael <rmendesduarte@gmail.com>
2016-06-15 10:52:48 -0300

    add labels to cal and val settings

--- screens/cal/CalSettings.py
+++ screens/cal/CalSettings.py
@@ -17,6 +17,46 @@ class CalSettings(Screen):
 
         boxg = BoxLayout(padding=10, spacing=10, orientation='vertical')
 
+        ## TOP PART
+
+        box_top = BoxLayout(size_hint_x=1, size_hint_y=0.3,padding=10, spacing=10, orientation='vertical')
+
+        box_nt = BoxLayout(orientation = 'horizontal')
+        label_nt = Label(text = 'Number of Trials', font_size=FONT_SIZE)
+        self.n_trials = TextInput(size_hint=(1, 0.8), font_size= FONT_SIZE,
+                        text='50', multiline=False)
+        box_nt.add_widget(label_nt)
+        box_nt.add_widget(self.n_trials)
+        box_top.add_widget(box_nt)
+
+        box_po = BoxLayout(orientation = 'horizontal')
+        label_po = Label(text = 'Pause Offset (s)', font_size=FONT_SIZE)
+        self.pause_offset = TextInput(size_hint=(1, 0.8), font_size= FONT_SIZE,
+                        text='1', multiline=False)
+        box_po.add_widget(label_po)
+        box_po.add_widget(self.pause_offset)
+        box_top.add_widget(box_po)
+
+        box_co = BoxLayout(orientation = 'horizontal')
+        label_co = Label(text = 'Cue Offset (s)', font_size=FONT_SIZE)
+        self.cue_offset = TextInput(size_hint=(1, 0.8), font_size= FONT_SIZE,
+                        text='3', multiline=False)
+        box_co.add_widget(label_co)
+        box_co.add_widget(self.cue_offset)
+        box_top.add_widget(box_co)
+
+        box_eto = BoxLayout(orientation = 'horizontal')
+        label_eto = Label(text = 'End Of Trial Offset (s)', font_size=FONT_SIZE)
+        self.end_trial_offset = TextInput(size_hint=(1, 0.8), font_size= FONT_SIZE,
+                        text='8', multiline=False)
+        box_eto.add_widget(label_eto)
+        box_eto.add_widget(self.end_trial_offset)
+        box_top.add_widget(box_eto)
+
+        boxg.add_widget(box_top)
+
+        ## BOTTOM PART
+
         box_bottom = BoxLayout(size_hint_x=1, size_hint_y=0.3,padding=10, spacing=10, orientation='vertical')
 
         self.label_msg = Label(text="", font_size=FONT_SIZE)
@@ -27,31 +67,10 @@ class CalSettings(Screen):
         button_back = Button(text="Back", size = BUTTON_SIZE)
         button_back.bind(on_press= self.change_to_cal)
 
-        box_top = BoxLayout(size_hint_x=1, size_hint_y=0.3,padding=10, spacing=10, orientation='vertical')
-
-        self.n_trials = TextInput(size_hint=(1, 0.8), font_size= FONT_SIZE,
-                        hint_text='Number of Trials', multiline=False)
-
-        self.pause_offset = TextInput(size_hint=(1, 0.8), font_size= FONT_SIZE,
-                        hint_text='Pause Offset', multiline=False)
-
-        self.cue_offset = TextInput(size_hint=(1, 0.8), font_size= FONT_SIZE,
-                        hint_text='Cue Offset', multiline=False)
-
-        self.end_trial_offset = TextInput(size_hint=(1, 0.8), font_size= FONT_SIZE,
-                        hint_text='End of Trial Offset', multiline=False)
-
-
-        box_top.add_widget(self.n_trials)
-        box_top.add_widget(self.pause_offset)
-        box_top.add_widget(self.cue_offset)
-        box_top.add_widget(self.end_trial_offset)
-
         box_bottom.add_widget(self.label_msg)
         box_bottom.add_widget(button_save)
         box_bottom.add_widget(button_back)
 
-        boxg.add_widget(box_top)
         boxg.add_widget(box_bottom)
 
         self.add_widget(boxg)
--- screens/val/ValSettings.py
+++ screens/val/ValSettings.py
@@ -16,6 +16,46 @@ class ValSettings(Screen):
 
         boxg = BoxLayout(padding=10, spacing=10, orientation='vertical')
 
+        ## TOP PART
+
+        box_top = BoxLayout(size_hint_x=1, size_hint_y=0.3,padding=10, spacing=10, orientation='vertical')
+
+        box_nt = BoxLayout(orientation = 'horizontal')
+        label_nt = Label(text = 'Number of Trials', font_size=FONT_SIZE)
+        self.n_trials = TextInput(size_hint=(1, 0.8), font_size= FONT_SIZE,
+                        hint_text='Using the same as cal', multiline=False)
+        box_nt.add_widget(label_nt)
+        box_nt.add_widget(self.n_trials)
+        box_top.add_widget(box_nt)
+
+        box_po = BoxLayout(orientation = 'horizontal')
+        label_po = Label(text = 'Pause Offset (s)', font_size=FONT_SIZE)
+        self.pause_offset = TextInput(size_hint=(1, 0.8), font_size= FONT_SIZE,
+                        hint_text='Using the same as cal', multiline=False)
+        box_po.add_widget(label_po)
+        box_po.add_widget(self.pause_offset)
+        box_top.add_widget(box_po)
+
+        box_co = BoxLayout(orientation = 'horizontal')
+        label_co = Label(text = 'Cue Offset (s)', font_size=FONT_SIZE)
+        self.cue_offset = TextInput(size_hint=(1, 0.8), font_size= FONT_SIZE,
+                        hint_text='Using the same as cal', multiline=False)
+        box_co.add_widget(label_co)
+        box_co.add_widget(self.cue_offset)
+        box_top.add_widget(box_co)
+
+        box_eto = BoxLayout(orientation = 'horizontal')
+        label_eto = Label(text = 'End Of Trial Offset (s)', font_size=FONT_SIZE)
+        self.end_trial_offset = TextInput(size_hint=(1, 0.8), font_size= FONT_SIZE,
+                        hint_text='Using the same as cal', multiline=False)
+        box_eto.add_widget(label_eto)
+        box_eto.add_widget(self.end_trial_offset)
+        box_top.add_widget(box_eto)
+
+        boxg.add_widget(box_top)
+
+        ## BOTTOM PART
+
         box_bottom = BoxLayout(size_hint_x=1, size_hint_y=0.3,padding=10, spacing=10, orientation='vertical')
 
         self.label_msg = Label(text="", font_size=FONT_SIZE)
@@ -26,31 +66,10 @@ class ValSettings(Screen):
         button_back = Button(text="Back", size = BUTTON_SIZE)
         button_back.bind(on_press= self.change_to_cal)
 
-        box_top = BoxLayout(size_hint_x=1, size_hint_y=0.3,padding=10, spacing=10, orientation='vertical')
-
-        self.n_trials = TextInput(size_hint=(1, 0.8), font_size= FONT_SIZE,
-                        hint_text='Number of Trials', multiline=False)
-
-        self.pause_offset = TextInput(size_hint=(1, 0.8), font_size= FONT_SIZE,
-                        hint_text='Pause Offset', multiline=False)
-
-        self.cue_offset = TextInput(size_hint=(1, 0.8), font_size= FONT_SIZE,
-                        hint_text='Cue Offset', multiline=False)
-
-        self.end_trial_offset = TextInput(size_hint=(1, 0.8), font_size= FONT_SIZE,
-                        hint_text='End of Trial Offset', multiline=False)
-
-
-        box_top.add_widget(self.n_trials)
-        box_top.add_widget(self.pause_offset)
-        box_top.add_widget(self.cue_offset)
-        box_top.add_widget(self.end_trial_offset)
-
         box_bottom.add_widget(self.label_msg)
         box_bottom.add_widget(button_save)
         box_bottom.add_widget(button_back)
 
-        boxg.add_widget(box_top)
         boxg.add_widget(box_bottom)
 
         self.add_widget(boxg)
@@ -62,10 +81,27 @@ class ValSettings(Screen):
 
     def save_config(self,*args):
 
-        self.sh.v_n_trials = self.n_trials.text
-        self.sh.v_cue_offset =  self.cue_offset.text
-        self.sh.v_pause_offset =  self.pause_offset.text
-        self.sh.v_end_trial_offset =  self.end_trial_offset.text
+        if self.n_trials.text == '':
+            print 'using the same as in cal'
+            self.sh.v_n_trials = self.sh.c_n_trials
+        else:
+            self.sh.v_n_trials = self.n_trials.text
+
+        if self.cue_offset.text == '':
+            self.sh.v_cue_offset = self.sh.c_cue_offset
+        else:
+            self.sh.v_cue_offset =  self.cue_offset.text  
+
+        if self.pause_offset.text == '':
+            self.sh.v_pause_offset = self.sh.c_pause_offset
+        else:
+            self.sh.v_pause_offset =  self.pause_offset.text
+
+        if self.end_trial_offset.text == '':
+            self.sh.v_end_trial_offset = self.sh.c_end_trial_offset
+        else:
+            self.sh.v_end_trial_offset =  self.end_trial_offset.text
+
 
         self.sh.data_val_path = PATH_TO_SESSION + self.sh.name + '/' + 'data_val.txt'
         self.sh.events_val_path = PATH_TO_SESSION + self.sh.name + '/' + 'events_val.txt'
