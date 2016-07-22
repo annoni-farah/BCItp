741763d78bb35f629d0162e847d1cdc2c3dcaf2c
rafael <rafaelmd@attosistemas.com.br>
2016-07-22 16:38:00 -0300

    first commit with kv testing

--- screens/ScreenStart.py
+++ screens/ScreenStart.py
@@ -4,6 +4,9 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
+from kivy.lang import Builder
+
+from kivy.graphics import Rectangle, Color
 
 import os
 
@@ -12,7 +15,7 @@ import json
 from standards import *
 from kivy_utils import ErrorPopup
 
-TEXT_SIZE = (1, 0.7)
+Builder.load_file('/arquivos/mestrado/repo/bci_training_platform/screens/screenstart.kv')
 
 class StartScreen(Screen):
 # layout
@@ -21,37 +24,34 @@ class StartScreen(Screen):
 
         self.sh = session_header
 
-        boxg = AnchorLayout(anchor_x='center', anchor_y='center')
+        # boxg = AnchorLayout(anchor_x='center', anchor_y='center')
 
-        box1 = BoxLayout(size_hint = (0.5, 0.5), padding=10, spacing=10,
-                         orientation='vertical')
+        # box1 = BoxLayout(padding=10, spacing=10, orientation='vertical')
 
-        self.label_msg = Label(text="", font_size=FONT_SIZE)
+        # self.label_msg = Label(size_hint=(None,None), text="", font_size=FONT_SIZE)
 
-        button_ui_settings = Button(text="General Settings", font_size = FONT_SIZE,
-            size = BUTTON_SIZE)
-        button_ui_settings.bind(on_press= self.change_to_gen_settings)
+        # button_ui_settings = Button(size_hint=(None,None), text="General Settings", font_size = FONT_SIZE,
+        #     size = BUTTON_SIZE)
+        # button_ui_settings.bind(on_press=self.change_to_gen_settings)
 
-        button_next = Button(text="BCI Menu", size=BUTTON_SIZE, font_size = FONT_SIZE)
-        button_next.bind(on_press= self.change_to_bci)
+        # button_next = Button(size_hint = (None,None), text="BCI Menu", size=BUTTON_SIZE, font_size = FONT_SIZE)
+        # button_next.bind(on_press= self.change_to_bci)
 
-        button_save = Button(text="Save", size=BUTTON_SIZE, font_size = FONT_SIZE)
-        button_save.bind(on_press= self.save_session_name)
+        # button_save = Button(size_hint = (None,None), text="Save", size=BUTTON_SIZE, font_size = FONT_SIZE)
+        # button_save.bind(on_press= self.save_session_name)
 
-        self.session_name = TextInput(font_size= FONT_SIZE, size_hint=TEXT_SIZE,
-            hint_text='Session Name', multiline=False)
+        # self.session_name = TextInput(size_hint = (None,None), font_size= FONT_SIZE, size=BUTTON_SIZE,
+        #     hint_text='Session Name', multiline=False)
 
-        box1.add_widget(self.label_msg)
-        box1.add_widget(self.session_name)
-        box1.add_widget(button_save)
-        box1.add_widget(button_ui_settings)
-        box1.add_widget(button_next)
+        # box1.add_widget(self.label_msg)
+        # box1.add_widget(button_save)
+        # box1.add_widget(button_ui_settings)
+        # box1.add_widget(self.session_name)
+        # box1.add_widget(button_next)
 
-        # box1.size = box1.minimum_size
+        # boxg.add_widget(box1)
 
-        boxg.add_widget(box1)
-
-        self.add_widget(boxg)
+        # self.add_widget(boxg)
 
     def change_to_gen_settings(self,*args):
         self.manager.current = 'GeneralSettings'
