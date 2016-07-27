178ac97d1c07bb022943f1f88dea1595ee2fa5e3
rafael <rafaelmendesduarte@hotmail.com>
2016-07-27 19:23:19 -0300

    add dataprocessing settings in kv

--- screens/settings/DataProcessingSettings.py
+++ screens/settings/DataProcessingSettings.py
@@ -1,117 +1,46 @@
+############################## DEPENDENCIES ##########################
+# KIVY modules:
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
-from kivy.uix.checkbox import CheckBox
+from kivy.properties import ObjectProperty, NumericProperty, StringProperty, ReferenceListProperty, \
+                            ListProperty, BooleanProperty
+from kivy.lang import Builder
 
-from utils import saveObjAsJson
+# KV file:
+Builder.load_file('screens/settings/dataprocessingsettings.kv')
 
+# Generic:
+
+# Project's:
+from SampleManager import *
 from standards import *
+from approach import Approach
+######################################################################
+
 
 class DataProcessingSettings(Screen):
+
+    buf_len = ObjectProperty(None)
+    channels = ObjectProperty(None)
+    f_high = ObjectProperty(None)
+    f_low = ObjectProperty(None)
+    f_order = ObjectProperty(None)
+
+    msg = StringProperty('')
+
+    notch = BooleanProperty(False)
+
 # layout
     def __init__ (self, session_header,**kwargs):
         super (DataProcessingSettings, self).__init__(**kwargs)
         self.sh = session_header
-
-        boxg = BoxLayout(orientation='vertical', padding=10, spacing=10)
-
-        self.label_msg = Label(text="", font_size=FONT_SIZE)
-        ## BOTTOM PART (BUTTONS)
-
-        box_bottom = BoxLayout(size_hint= BUTTON_BOX_SIZE,padding=10, spacing=10, orientation='vertical')
-
-        button_save = Button(text="Save", size = BUTTON_SIZE)
-        button_save.bind(on_press= self.save_config)
-
-        button_back = Button(text="Back", size = BUTTON_SIZE)
-        button_back.bind(on_press= self.change_to_cal)
-
-        box_bottom.add_widget(self.label_msg)
-        box_bottom.add_widget(button_save)
-        box_bottom.add_widget(button_back)
-
-        boxg.add_widget(box_bottom)
-
-        ## TOP PART
-
-        box_top = BoxLayout(size_hint_x=1, size_hint_y=0.7,padding=10, spacing=10, orientation='vertical')
-
-        # DATA CONFIG
-        box_data = BoxLayout(size_hint_x=1, size_hint_y=0.35,padding=10, spacing=10, orientation='vertical')
-
-        box_buf = BoxLayout(orientation = 'horizontal')
-        label_buf = Label(text ='Circular Buffer Length', font_size=FONT_SIZE)
-        self.buf_len = TextInput(size_hint=(1, 0.8), font_size= FONT_SIZE, text='500', multiline=False)
-        box_buf.add_widget(label_buf)
-        box_buf.add_widget(self.buf_len)
-
-        box_ch = BoxLayout(orientation = 'horizontal')
-        label_ch = Label(text = 'Channels idx used', font_size=FONT_SIZE)
-        self.channels = TextInput(size_hint=(1, 0.8), font_size= FONT_SIZE,
-                text='0 1 2 3 4 5 6 7', multiline=False)
-        box_ch.add_widget(label_ch)
-        box_ch.add_widget(self.channels)
-
-        box_data.add_widget(box_buf)
-        box_data.add_widget(box_ch)
-
-        box_top.add_widget(box_data)
-
-
-        # FILTER CONFIG
-        box_filter = BoxLayout(size_hint_x=1, size_hint_y=0.65,padding=10, spacing=10, orientation='vertical')
-
-        # box_f_band = BoxLayout(size_hint_x=1,padding=10, spacing=10, orientation='vertical')
-
-        box_high = BoxLayout(orientation = 'horizontal')
-        label_high = Label(text = 'Upper cutoff freq (Hz)', font_size=FONT_SIZE)
-        self.f_high = TextInput(size_hint=(1, 0.8), font_size= FONT_SIZE, text='30', multiline=False)
-        box_high.add_widget(label_high)
-        box_high.add_widget(self.f_high)
-
-        box_low = BoxLayout(orientation = 'horizontal')
-        label_low = Label(text = 'Lower cutoff freq (Hz)', font_size=FONT_SIZE)
-        self.f_low = TextInput(size_hint=(1, 0.8), font_size= FONT_SIZE, text='8', multiline=False)
-        box_low.add_widget(label_low)
-        box_low.add_widget(self.f_low)
-
-        box_filter.add_widget(box_low)
-        box_filter.add_widget(box_high)
-        # box_filter.add_widget(box_f_band)
-
-
-        box_order = BoxLayout(orientation = 'horizontal')
-        label_order = Label(text = 'Filter Order', font_size=FONT_SIZE)
-        self.f_order = TextInput(size_hint=(1, 0.8), font_size= FONT_SIZE, text='7', multiline=False)
-        box_order.add_widget(label_order)
-        box_order.add_widget(self.f_order)
-
-        box_filter.add_widget(box_order)
-
-        box_notch_checkbox = BoxLayout(orientation='horizontal')
-        label_notch = Label(text="Use Notch Filter", font_size=FONT_SIZE)
-        checkbox_notch = CheckBox(active = True)
-        checkbox_notch.bind(active=self.enable_notch_filt)
-        self.notch = True
-
-        box_notch_checkbox.add_widget(label_notch)
-        box_notch_checkbox.add_widget(checkbox_notch)
-
-        box_filter.add_widget(box_notch_checkbox)
-
-        box_top.add_widget(box_filter)
-
-        boxg.add_widget(box_top, 2)
-
-        self.add_widget(boxg)
 
     def change_to_cal(self,*args):
         self.manager.current = 'BCIMenu'
         self.manager.transition.direction = 'right'
 
     def enable_notch_filt(self, checkbox, value):
+        
+        print 'val:', value
         if value:
             self.notch = True
         else:
@@ -128,4 +57,4 @@ class DataProcessingSettings(Screen):
         self.sh.notch = self.notch
 
         self.sh.saveToPkl()
-        self.label_msg.text = "Settings Saved!"
+        self.msg = "Settings Saved!"
