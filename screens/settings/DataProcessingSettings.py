816487ac849d7c22a425d3e6c96ca03865d3db42
rafael <rafaelmendesduarte@hotmail.com>
2016-08-17 12:46:57 -0300

    remove notch option; add self update settings

--- screens/settings/DataProcessingSettings.py
+++ screens/settings/DataProcessingSettings.py
@@ -41,9 +41,21 @@ class DataProcessingSettings(Screen):
             limits=map(int,ids.channels.value.split(':'))
             ch_idx = range(limits[0],limits[1])
         else:
-            ch_idx = map(int,ids.channels.value.split(" "))
+            ch_idx = map(int,ids.channels.value.split(' '))
 
         self.sh.channels = ch_idx
 
         self.sh.saveToPkl()
         self.msg = "Settings Saved!"
+
+    def update_settings(self):
+
+        ids = self.ids
+
+        ids.buf_len.value= self.sh.buf_len
+        ids.f_low.value = self.sh.f_low
+        ids.f_high.value = self.sh.f_high
+        ids.f_order.value = self.sh.f_order
+
+        # not the best option but it works
+        ids.channels.value = str(self.sh.channels).replace(',','').replace('[', '').replace(']', '')
