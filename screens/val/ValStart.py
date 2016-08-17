90822e73f0ee4967f9533eef2b9205af3c708123
rafael <rafaelmendesduarte@hotmail.com>
2016-08-17 17:45:07 -0300

    add validation screen kv file

--- screens/val/ValStart.py
+++ screens/val/ValStart.py
@@ -1,81 +1,67 @@
+############################## DEPENDENCIES ##########################
+# KIVY modules:
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.button import Button
+from kivy.properties import ObjectProperty, NumericProperty, StringProperty, ReferenceListProperty, \
+                            ListProperty, BooleanProperty
+from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
-
+from kivy.uix.popup import Popup
 from kivy.clock import Clock
 
-# from threading import Thread
+
+
+# KV file:
+Builder.load_file('screens/val/valstart.kv')
+
+# Generic:
+import random
+import json
+import os
+
+# Project's:
+from standards import *
+from approach import Approach
 from SampleManager import *
 
-from kivy.uix.carousel import Carousel
-from kivy.uix.image import AsyncImage
+######################################################################
 
-from kivy.graphics import Rectangle, Color
-
-import random
-
-import json
-
-from standards import *
 
 class ValStart(Screen):
+
+    src = ["data/resources/cross.png", \
+       "data/resources/left.png", \
+       "data/resources/right.png", \
+       "data/resources/blank.png"]
+
+    fig_list = ListProperty(src)
+    button_stream = StringProperty('Start Streaming')
+
+    carousel = ObjectProperty(None)
+
 # layout
     def __init__ (self, session_header,**kwargs):
         super (ValStart, self).__init__(**kwargs)
+
         self.sh = session_header
-
-        box = BoxLayout(size_hint_x=1, size_hint_y=1,padding=10, spacing=10, orientation='vertical')
-
-        box1 = BoxLayout(size_hint_x=1, size_hint_y=0.5,padding=10, spacing=10, orientation='vertical')
-        box2 = BoxLayout(size_hint_x=1, size_hint_y=0.5,padding=10, spacing=10, orientation='vertical')
-
-        button_back = Button(text="Back", size = BUTTON_SIZE)
-        button_back.bind(on_press= self.change_to_val)
-
-        self.button_stream = Button(text="Start Streaming", size = BUTTON_SIZE)
-        self.button_stream.bind(on_press= self.start)
-
-        self.carousel = Carousel(direction='right')
-
-        src = ["data/resources/cross.png",
-               "data/resources/left.png",
-               "data/resources/right.png",
-               "data/resources/blank.png"]
-
-        for i in range(len(src)):
-            image = AsyncImage(source=src[i], allow_stretch=False)
-            self.carousel.add_widget(image)
-
-        box2.add_widget(self.carousel)
-        box1.add_widget(self.button_stream)
-        box1.add_widget(button_back)
-
-        box.add_widget(box2) 
-        box.add_widget(box1) 
-
-        self.add_widget(box)
-
         self.stream_flag = False
 
     def change_to_val(self,*args):
         self.manager.current = 'ValMenu'
         self.manager.transition.direction = 'right'
 
-    def start(self,*args):
+    def toggle_stream(self,*args):
 
         if self.stream_flag:
             self.stream_stop()
         else:
             self.stream_start()
 
-
     def stream_stop(self):
         self.sm.stop_flag = True
         self.stream_flag = False
         self.sm.join()
-        self.button_stream.text = 'Start Streaming'
+        self.button_stream = 'Start Streaming'
         self.clock_unscheduler()
         self.save_data()
 
@@ -87,12 +73,13 @@ class ValStart(Screen):
         self.sm.daemon = True  
         self.sm.stop_flag = False
         self.sm.start()
-        self.button_stream.text = 'Stop Streaming'
+        self.button_stream = 'Stop Streaming'
         self.stream_flag = True
         self.clock_scheduler()
 
     def clock_scheduler(self):
         Clock.schedule_interval(self.display_epoch, self.sh.end_trial_offset)
+
 
     def clock_unscheduler(self):
         Clock.unschedule(self.display_epoch)
@@ -135,5 +122,4 @@ class ValStart(Screen):
     def generate_stim_list(self):
         self.stim_list =  [random.randrange(1, 3) for _ in range(0, self.sh.n_trials)]
         self.epoch_counter = 0
-
 
