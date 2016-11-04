614d42a154907d11ead41b85344159fa5d9e8b7e
Rafael <rmendesduarte@gmail.com>
2016-11-04 10:04:18 -0200

    add latest working ver

--- signal_processing/examples/plot_drone_path.py
+++ signal_processing/examples/plot_drone_path.py
@@ -2,33 +2,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-CHECKPOINTx = [0, 0, -20, -20, 20]
+# CHECKPOINTx = [0, 0, -20, -20, 20]
+# CHECKPOINTy = [-20, 0, 0, 20, 20]
+
+CHECKPOINTx = [0, 0, 20, 20, -20]
 CHECKPOINTy = [-20, 0, 0, 20, 20]
 
 r_sum = None
-N = [1, 2, 3, 4, 5, 8, 9, 10]
+# N = [1, 2, 3, 4, 5, 8, 9, 10]
 runtime = []
-for i in N:
+N_sample = 20
+N = []
+
+plt.subplot(2, 1, 1)
+
+for i in range(N_sample):
     RESULTS_PATH = '/home/rafael/codes/bci_training_platform/data/' + \
-        'session/A4_comp_drone/drone_runs_official/game_data_run' + \
+        'session/A4_comp_drone/drone_runs_trofino_simu2/game_data_run' + \
         str(i) + '.npy'
+    try:
+        d = np.load(RESULTS_PATH)
+        r = d[0]
+        r = r[1:]  # remove first point
 
-    d = np.load(RESULTS_PATH)
-    r = d[0]
-    r = r[1:]  # remove first point
+        plt.plot(r[:, 0], r[:, 1])
 
-    plt.plot(r[:, 0], r[:, 1])
+        if r_sum is None:
+            r_sum = np.zeros([672, 2])
 
-    if r_sum is None:
-        r_sum = np.zeros([554, 2])
+        r_sum = np.sum([r_sum, r[:r_sum.shape[0], :]], axis=0)
 
-    r_sum = np.sum([r_sum, r[:r_sum.shape[0], :]], axis=0)
+        t = d[1]
 
-    t = d[1]
+        runtime.append(t)
+        N.append(i)
 
-    runtime.append(t)
+    except Exception as inst:
+        print inst
+        N_sample -= 1
 
-r_tot = r_sum / (len(N))
+
+r_tot = r_sum / (N_sample)
 time_avg = sum(runtime) / len(runtime)
 
 
@@ -36,23 +50,25 @@ time_avg = sum(runtime) / len(runtime)
 plt.plot(r_tot[:, 0], r_tot[:, 1], 'k', linewidth=4.0, label='Mean')
 plt.plot(CHECKPOINTx, CHECKPOINTy, 'ro')
 plt.axis([-60, 60, -60, 60])
+plt.axis('equal')
 plt.ylabel('Pos Y (m)')
 plt.xlabel('Pos X (m)')
 plt.grid(True)
-plt.legend()
-
-plt.show()
+plt.legend(loc=0)
 
 # PLOT RUNTIME
 my_xticks = [str(i) for i in N]
 x = range(len(N))
 tavg = np.empty([len(N)])
 tavg[:] = time_avg
+plt.subplot(2, 1, 2)
 plt.xticks(x, my_xticks)
 plt.bar(x, runtime, align='center', alpha=0.5)
 plt.plot(x, tavg, 'k', linewidth=4.0, label='Mean')
+# plt.axis([-60, 60, -60, 60]
 plt.xlabel('Run Number')
 plt.ylabel('Time (s)')
-plt.legend()
+plt.legend(loc=0)
 plt.grid(True)
+
 plt.show()
