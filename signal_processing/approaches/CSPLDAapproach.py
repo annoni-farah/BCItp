0297dfb0a15ecf7f655d7cd894a90d17cf9a67e6
Rafael <rmendesduarte@gmail.com>
2016-11-25 16:47:19 -0200

    change func names

--- signal_processing/approaches/CSPLDAapproach.py
+++ signal_processing/approaches/CSPLDAapproach.py
@@ -1,71 +1,57 @@
 """CSP + LDA approach.
 Implements the CSP + LDA approach using a data from the V BCI competition
 """
-import os, sys, inspect
 
-cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split \
-	(inspect.getfile( inspect.currentframe() ))[0],'../')))
-if cmd_subfolder not in sys.path:
-    sys.path.insert(0, cmd_subfolder)
-
-from DataManipulation import *
-from DataProcessing import *
-
-from processing_utils import loadBiosig, nanCleaner
-
-import numpy as np
+from .handler import load_data_as_np, read_events, extract_epochs
+from .processor import Filter, Learner
 
 import math
 
-### MAIN ###
-def apply_ml(DATA_CAL_PATH, CAL_EVENTS_PATH, DATA_VAL_PATH, VAL_EVENTS_PATH, 
-		SAMPLING_FREQ, LOWER_CUTOFF, UPPER_CUTOFF, FILT_ORDER, CSP_N, EVENT_IDS, T_MIN, T_MAX):
 
-	### MAIN ###
+def apply_ml(DATA_CAL_PATH, CAL_EVENTS_PATH, DATA_VAL_PATH, VAL_EVENTS_PATH,
+             SAMPLING_FREQ, LOWER_CUTOFF, UPPER_CUTOFF, FILT_ORDER,
+             CSP_N, EVENT_IDS, T_MIN, T_MAX):
 
-	dp = Filter(LOWER_CUTOFF, UPPER_CUTOFF, SAMPLING_FREQ, FILT_ORDER)
+    dp = Filter(LOWER_CUTOFF, UPPER_CUTOFF, SAMPLING_FREQ, FILT_ORDER)
 
-	# # LOAD CALIBRATION DATA:
-	# if DATA_CAL_PATH[-3:] == 'gdf':
-	# 	data_cal, SAMPLING_FREQ = loadBiosig(DATA_CAL_PATH).T
-	# 	data_cal = nanCleaner(data_cal)
+    # # LOAD CALIBRATION DATA:
+    # if DATA_CAL_PATH[-3:] == 'gdf':
+    # 	data_cal, SAMPLING_FREQ = loadBiosig(DATA_CAL_PATH).T
+    # 	data_cal = nanCleaner(data_cal)
 
-	# else:
-	data_cal = loadDataAsMatrix(DATA_CAL_PATH).T
-	
-	events_list_cal = readEvents(CAL_EVENTS_PATH)
+    # else:
+    data_cal = load_data_as_np(DATA_CAL_PATH).T
 
-	data_cal = dp.ApplyFilter(data_cal)
+    events_list_cal = read_events(CAL_EVENTS_PATH)
 
-	# FEATURE EXTRACTION:
-	SMIN = int(math.floor(T_MIN * SAMPLING_FREQ))
-	SMAX = int(math.floor(T_MAX * SAMPLING_FREQ))
+    data_cal = dp.apply_filter(data_cal)
 
-	epochs_cal, labels_cal = extractEpochs(data_cal, events_list_cal, SMIN, SMAX)
+    # FEATURE EXTRACTION:
+    SMIN = int(math.floor(T_MIN * SAMPLING_FREQ))
+    SMAX = int(math.floor(T_MAX * SAMPLING_FREQ))
 
-	dl = Learner()
+    epochs_cal, labels_cal = extract_epochs(
+        data_cal, events_list_cal, SMIN, SMAX)
 
-	dl.DesignLDA()
-	dl.DesignCSP(CSP_N)
-	dl.AssembleLearner()
-	dl.Learn(epochs_cal, labels_cal)
+    dl = Learner()
 
-	data_val = loadDataAsMatrix(DATA_VAL_PATH).T
+    dl.design_LDA()
+    dl.design_CSP(CSP_N)
+    dl.assemble_learner()
+    dl.learn(epochs_cal, labels_cal)
 
-	events_list_val = readEvents(VAL_EVENTS_PATH)
+    data_val = load_data_as_np(DATA_VAL_PATH).T
 
-	data_val = dp.ApplyFilter(data_val)
+    events_list_val = read_events(VAL_EVENTS_PATH)
 
-	# FEATURE EXTRACTION:
-	epochs_val, labels_val = extractEpochs(data_val, events_list_val, SMIN, SMAX)
+    data_val = dp.apply_filter(data_val)
 
-	dl.EvaluateSet(epochs_val, labels_val)
+    # FEATURE EXTRACTION:
+    epochs_val, labels_val = extract_epochs(
+        data_val, events_list_val, SMIN, SMAX)
 
-	dl.PrintResults()
+    dl.evaluate_set(epochs_val, labels_val)
 
-	return dl.GetResults()
+    dl.print_results()
 
-
-
-
-
+    return dl.get_results()
