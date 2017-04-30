f011239c66106ee79e230073031b447835e3ba2d
Rafael <rmendesduarte@gmail.com>
2017-04-30 13:22:40 +0200

    add setup.py for package setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='BCItp',
+    version='0.0.1',
+    packages=['bcitp', ],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
