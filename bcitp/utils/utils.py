0e6e16484769fcac07d6ad5d8ea39488af5556c1
Rafael <rmendesduarte@gmail.com>
2017-04-30 22:09:36 +0200

    organize utils file

--- bcitp/utils/utils.py
+++ bcitp/utils/utils.py
@@ -1,64 +1,48 @@
-import numpy as np
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-import json
+# BCItp
+#
+# Copyright (C) 2014-2017 BCItp Project
+# Author: Rafael Duarte <rmendesduarte@gmail.com>
+# URL: <http://bcitp.readthedocs.io/>
+# For license information, see LICENSE.TXT
+
+import numpy as np
 
 
 def load_as_matrix(path):
-    """Loads text file content as numpy matrix
-    Parameters
-    ----------
-    path : path to text file
+    '''
+        Loads text file content as numpy matrix
 
-    cols : order of columns to be read
+        :param path: path to text file
 
-    Returns
-    -------
-    matrix : numpy matrix, shape as written in txt
+        :param cols: order of columns to be read
 
-    Examples
-    --------
-    >>> data_path = "/PATH/TO/FILE/somematrix.txt"
-    >>> matrix_data = loadAsMatrix(data_path)
-    """
+        Returns
+        -------
+        matrix : numpy matrix, shape as written in txt
+
+        Examples
+        --------
+        >>> data_path = "/PATH/TO/FILE/somematrix.txt"
+        >>> matrix_data = loadAsMatrix(data_path)
+    '''
 
     matrix = np.load(open(path, "rb"))
 
-    # return np.fliplr(matrix.T).T
     return matrix
 
 
-def clean_nan(data_in):
-    """Removes NaN from data by interpolation
-    Parameters
-    ----------
-    data_in : input data - np matrix channels x samples
-
-    Returns
-    -------
-    data_out : clean dataset with no NaN samples
-
-    Examples
-    --------
-    >>> data_path = "/PATH/TO/DATASET/dataset.gdf"
-    >>> EEGdata_withNaN = loadBiosig(data_path)
-    >>> EEGdata_clean = clean_nan(EEGdata_withNaN)
-    """
-    for i in range(data_in.shape[0]):
-
-        bad_idx = np.isnan(data_in[i, ...])
-        data_in[i, bad_idx] = np.interp(bad_idx.nonzero()[0],
-                                        (~bad_idx).nonzero()[0],
-                                        data_in[i, ~bad_idx])
-
-    return data_in
-
-
 def save_matrix_as_NPY(data_in, path, mode='a'):
+    '''
+        Saves a numpy format matrix into a .npy file
+
+        :param data_in: numpy matrix
+        :param path: path to save file
+        :param mode: open method a-append, w-write(optional)
+
+    '''
 
     with open(path, mode) as data_file:
         np.save(data_file, data_in)
-
-
-def save_obj_as_json(obj, filename):
-    with open(filename, "w") as file:
-        file.write(json.dumps(obj.__dict__, file, indent=4))
