855c6418868b555630181fddece393e236c1f693
Rafael <rmendesduarte@gmail.com>
2017-04-30 14:32:19 +0200

    organize event handler file

--- bcitp/handlers/event_handler.py
+++ bcitp/handlers/event_handler.py
@@ -1,37 +1,46 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# from threading import Thread
-import numpy as np
+# BCItp
+#
+# Copyright (C) 2014-2017 BCItp Project
+# Author: Rafael Duarte <rmendesduarte@gmail.com>
+# URL: <http://bcitp.readthedocs.io/>
+# For license information, see LICENSE.TXT
 
+# KIVY PACKAGES
+from kivy.logger import Logger
+
+import numpy as np
 
 from utils import save_matrix_as_NPY
 
 
 class EventHandler():
+    '''
+        A Class for handling the recording and storing of events.
+    '''
 
     def __init__(self):
         super(EventHandler, self).__init__()
 
-    def StoreEvents(self, new_data):
+    def mark_events(self, ev_type):
+        '''
+            Accumulates(appends) new events to event list. The event label
+            is grouped with its time stamp
 
-        data = np.array(new_data)  # transform list into numpy array
-
-        if not hasattr(self, 'all_data'):
-            self.all_data = np.array([]).reshape(0, len(data))
-
-        self.all_data = np.vstack(
-            (self.all_data, data))  # append to data stack
-
-    def MarkEvents(self, ev_type):
-
+            :param ev_type: event label
+        '''
+        Logger.info('Marked event ' + ev_type +
+                    'at sample ' + self.sample_counter)
         new = np.array([self.sample_counter, ev_type])
         self.event_list = np.vstack((self.event_list, new))
 
-    def SaveEvents(self, path):
+    def save_events(self, path):
+        '''
+            Stores the event list in the local disk
 
+            :param path: path to saved file
+        '''
+        Logger.info('Saving Events at ' + path)
         save_matrix_as_NPY(self.event_list, path, mode='w')
-
-    def clear_buffer(self):
-        self.circBuff.clear()
-        self.tBuff.clear()
