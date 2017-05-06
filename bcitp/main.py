85eb1a4d2130b317b631cd837255f3eab1cccec0
Rafael <rmendesduarte@gmail.com>
2017-05-06 16:01:41 +0200

    add main file to start package

--- bcitp/main.py
+++ bcitp/main.py
@@ -8,24 +8,25 @@ import os
 
 from kivy.uix.screenmanager import ScreenManager
 
-from data_headers.session_info import SessionHeader
+from .data_headers.session_info import SessionHeader
 
-from screens.start_screen import StartScreen
-from screens.menus import BCIMenu, CalMenu, GameMenu, DroneMenu
+from .screens.menus.start_menu import StartScreen
 
-from screens.settings.acquisition_settings import AcquisitionSettings
+# from screens.menus import BCIMenu, CalMenu, GameMenu, DroneMenu
 
-from screens.settings.cal_settings import CalSettings
-from screens.cal.cal_start import CalStart
+# from screens.settings.acquisition_settings import AcquisitionSettings
 
-from screens.ml.ml_screen import MlMenu
+# from screens.settings.cal_settings import CalSettings
+# from screens.cal.cal_start import CalStart
 
-from screens.game.game_settings import GameSettings
-from screens.game.bars_start import BarsStart
-from screens.game.target_start import TargetStart
+# from screens.ml.ml_screen import MlMenu
 
-from screens.game.drone_settings import DroneSettings
-from screens.game.drone_start import DroneStart
+# from screens.game.game_settings import GameSettings
+# from screens.game.bars_start import BarsStart
+# from screens.game.target_start import TargetStart
+
+# from screens.game.drone_settings import DroneSettings
+# from screens.game.drone_start import DroneStart
 
 # LOAD ALL KV FILES
 
@@ -41,7 +42,7 @@ def load_all_kv_files(start="screens/kv"):
         Builder.load_file(file_)
 
 
-class MyApp(App):
+class BCItp(App):
 
     def build(self):
         sh = SessionHeader()
@@ -49,50 +50,50 @@ class MyApp(App):
         sm = ScreenManager()
 
         # CREATE SCREENS
-        start_screen = StartScreen(sh, name='start')
-        settings_screen = GeneralSettings(sh, name='GeneralSettings')
-        bci_screen = BCIMenu(sh, name='BCIMenu')
+        start_menu = StartScreen(sh, name='start')
+        # settings_screen = GeneralSettings(sh, name='GeneralSettings')
+        # bci_screen = BCIMenu(sh, name='BCIMenu')
 
-        acquisition_settings_screen = AcquisitionSettings(
-            sh, name='AcquisitionSettings')
+        # acquisition_settings_screen = AcquisitionSettings(
+        #     sh, name='AcquisitionSettings')
 
-        cal_screen = CalMenu(sh, name='CalMenu')
-        cal_settings_screen = CalSettings(sh, name='CalSettings')
-        cal_start_screen = CalStart(sh, name='CalStart')
+        # cal_screen = CalMenu(sh, name='CalMenu')
+        # cal_settings_screen = CalSettings(sh, name='CalSettings')
+        # cal_start_screen = CalStart(sh, name='CalStart')
 
-        ml_screen = MlMenu(sh, name='MlMenu')
+        # ml_screen = MlMenu(sh, name='MlMenu')
 
-        game_screen = GameMenu(sh, name='GameMenu')
-        game_settings_screen = GameSettings(sh, name='GameSettings')
-        bars_start_screen = BarsStart(sh, name='BarsStart')
-        target_start_screen = TargetStart(sh, name='TargetStart')
+        # game_screen = GameMenu(sh, name='GameMenu')
+        # game_settings_screen = GameSettings(sh, name='GameSettings')
+        # bars_start_screen = BarsStart(sh, name='BarsStart')
+        # target_start_screen = TargetStart(sh, name='TargetStart')
 
-        ardrone_menu_screen = DroneMenu(sh, name='DroneMenu')
-        ardrone_settings_screen = DroneSettings(sh, name='DroneSettings')
-        ardrone_start_screen = DroneStart(sh, name='DroneStart')
+        # ardrone_menu_screen = DroneMenu(sh, name='DroneMenu')
+        # ardrone_settings_screen = DroneSettings(sh, name='DroneSettings')
+        # ardrone_start_screen = DroneStart(sh, name='DroneStart')
 
         # ADD SCREENS TO SCREEN MANAGER
-        sm.add_widget(start_screen)
+        sm.add_widget(start_menu)
 
-        sm.add_widget(settings_screen)
-        sm.add_widget(bci_screen)
+        # sm.add_widget(settings_screen)
+        # sm.add_widget(bci_screen)
 
-        sm.add_widget(acquisition_settings_screen)
+        # sm.add_widget(acquisition_settings_screen)
 
-        sm.add_widget(cal_screen)
-        sm.add_widget(cal_settings_screen)
-        sm.add_widget(cal_start_screen)
+        # sm.add_widget(cal_screen)
+        # sm.add_widget(cal_settings_screen)
+        # sm.add_widget(cal_start_screen)
 
-        sm.add_widget(ml_screen)
+        # sm.add_widget(ml_screen)
 
-        sm.add_widget(game_screen)
-        sm.add_widget(game_settings_screen)
-        sm.add_widget(bars_start_screen)
-        sm.add_widget(target_start_screen)
+        # sm.add_widget(game_screen)
+        # sm.add_widget(game_settings_screen)
+        # sm.add_widget(bars_start_screen)
+        # sm.add_widget(target_start_screen)
 
-        sm.add_widget(ardrone_menu_screen)
-        sm.add_widget(ardrone_settings_screen)
-        sm.add_widget(ardrone_start_screen)
+        # sm.add_widget(ardrone_menu_screen)
+        # sm.add_widget(ardrone_settings_screen)
+        # sm.add_widget(ardrone_start_screen)
 
         sm.current = 'start'
 
@@ -105,4 +106,4 @@ if __name__ == "__main__":
     # except Exception as e:
     #     print(e)
     load_all_kv_files()
-    MyApp().run()
+    BCItp().run()
